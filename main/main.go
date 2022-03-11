696aa02a2e97b284c9b30bb591b9ff781e58152c
yoonjeong <cyj199637@gmail.com>
2022-03-12 01:14:09 +0900

    21.03.12 Go For Beginners: 1.0 ~ 1.3

---
+++ main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	const greeting string = "Hello World!!!"
+	fmt.Println(greeting)
+
+	var greeting2 string = "Hello World!!!"
+	fmt.Println(greeting2)
+
+	greeting3 := "Hello World!!!"
+	fmt.Println(greeting3)
+}
